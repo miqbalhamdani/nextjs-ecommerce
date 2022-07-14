b37c4c187937e2897f0ed50c4c742eb1e5aed9b3
Iqbal Hamdani <miqbalh94@gmail.com>
2022-07-14 08:42:49 +0700

    [templating] Layouting, Header, and Footer.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import Default from '../components/layouts/Default'
+import '../assets/styles/styles.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Default>
+      <Component {...pageProps} />
+    </Default>
+  )
 }
 
 export default MyApp
